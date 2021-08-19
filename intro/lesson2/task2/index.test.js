da64cafa6dced30e65e5ea3136f09f76fcc0709f
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-08-20 00:34:01 +0300

    test2

--- intro/lesson2/task2/index.test.js
+++ intro/lesson2/task2/index.test.js
@@ -4,3 +4,11 @@ it('should get squared numbers', () => {
   const result = getSquaredNumbers([1, 2, 3]);
   expect(result).toEqual([1, 4, 9]);
 });
+it('should get Odd numbers', () => {
+  const result = getOddNumbers([1, 2, 3]);
+  expect(result).toEqual([1, 4, 9]);
+});
+it('should get Odd numbers', () => {
+  const result = sum(1, 2);
+  expect(result).toEqual(2, 4);
+});

84a6e43e221d3d01400c39e75f43e7adae3810a6
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-08-23 18:31:12 +0300

    tests

---
+++ lesson3/lesson4/reversed.js
@@ -0,0 +1,30 @@
+//reverse---------------------
+
+export const reverseArr = (arr) => {
+  if (!Array.isArray(arr)) {
+    return null;
+  }
+  const result = [...arr].reverse();
+  return result;
+};
+//withdraw----------------------
+
+export const withdraw = (clients, balances, client, amount) => {
+  let indexOfPerson = clients.indexOf(client);
+
+  if (balances[indexOfPerson] >= amount) {
+    return (balances[indexOfPerson] -= amount);
+  } else if (balances[indexOfPerson] <= amount) {
+    return -1;
+  }
+};
+// filtration object-------------
+export const getAdults = (obj) => {
+  const newObj = {};
+  for (let key in obj) {
+    if (obj[key] >= 18) {
+      newObj[key] = obj[key];
+    }
+  }
+  return newObj;
+};
---
+++ lesson3/lesson4/reversed.test.js
@@ -0,0 +1,46 @@
+//Reverse tests
+import { reverseArr } from './reversed';
+import { withdraw } from './reversed';
+import { getAdults } from './reversed';
+it('should reverse arr ', () => {
+  const result = reverseArr([1, 2, 3, 4]);
+  expect(result).toEqual([4, 3, 2, 1]);
+});
+
+it('should reverse arr', () => {
+  const result = reverseArr([100, 200, 300, 400]);
+  expect(result).toEqual([400, 300, 200, 100]);
+});
+
+it('should reverse arr', () => {
+  const result = reverseArr(['i', 'l', 'm']);
+  expect(result).toEqual(['m', 'l', 'i']);
+});
+//withdraw tests
+it('should return rest of many ', () => {
+  const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'John', 50);
+  expect(result).toEqual(37);
+});
+
+it('should reverse arr', () => {
+  const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -6], 'User', 10);
+  expect(result).toEqual(-1, [1400, 87, -6]);
+});
+it('should reverse arr', () => {
+  const result = withdraw(['Ann', 'John', 'User'], [1400, 87, -3], 'User', 22);
+  expect(result).toEqual(-1, [1400, 87, -6]);
+});
+
+//filtration object
+it('should return object wich contains persons > 18 ages', () => {
+  const result = getAdults({ 'John Doe': 19, Tom: 17, Bob: 18 });
+  expect(result).toEqual({ 'John Doe': 19, Bob: 18 });
+});
+it('should return object wich contains persons > 18 ages', () => {
+  const result = getAdults({ Ann: 56, Andrey: 7 });
+  expect(result).toEqual({ Ann: 56 });
+});
+it('should return object wich contains persons > 18 ages', () => {
+  const result = getAdults({ Bob: 21, Tom: 5 });
+  expect(result).toEqual({ Bob: 21 });
+});

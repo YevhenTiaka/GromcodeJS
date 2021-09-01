4379c1acac1553544762bbec59ea55f14068574d
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-01 20:25:36 +0300

    test7

--- DOM/task7/index.js
+++ DOM/task7/index.js
@@ -1,7 +1,7 @@
 export function createButton(buttonText) {
   const btn = document.createElement('button');
   btn.textContent = `${buttonText}`;
-  const btn2 = document.body.append(btn);
+  const addBtn = document.body.append(btn);
 }
 
 // createButton('Button');

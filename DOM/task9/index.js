db31cf992c91fe71b3b60288a68b93e631552551
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-01 23:33:05 +0300

    test9

--- DOM/task9/index.js
+++ DOM/task9/index.js
@@ -1,10 +1,9 @@
 export function finishForm() {
   const form = document.querySelector('form');
-  form.removeAttribute('class');
   form.setAttribute('name', 'login');
   form.setAttribute('class', 'login-form');
   const input = document.querySelector('input');
   input.setAttribute('type', 'password');
-  return input;
+  return form;
 }
-finishForm();
+console.log(finishForm());

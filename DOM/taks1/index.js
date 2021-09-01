644d090363e6ef0aaeefb3245e6948cba6cacc03
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-01 16:39:12 +0300

    test1

---
+++ DOM/taks1/index.js
@@ -0,0 +1,12 @@
+function getTitleElement() {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+}
+
+getTitleElement();
+
+function getInputElement() {
+  const inputElem = document.querySelectorAll('input');
+  console.dir(inputElem);
+}
+getInputElement();

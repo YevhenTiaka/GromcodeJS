324b7a3ac8240a7caac1d8781a3a420b723d3f8c
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-03 14:53:10 +0300

    test11

--- DOM/task11/index.js
+++ DOM/task11/index.js
@@ -1,6 +1,9 @@
 function squareNumbers() {
-  const numbersAtr = document.querySelectorAll('li[data-number]');
-  console.log();
+  const numbersAtr = document.querySelectorAll('li');
+  const arrayNumbers = Array.from(numbersAtr);
+  arrayNumbers.map((elem) => {
+    elem.dataset.squaredNumber = elem.dataset.number * elem.dataset.number;
+  });
 }
 
 squareNumbers();

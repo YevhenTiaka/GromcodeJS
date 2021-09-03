36ceb763b2081a657774e31fd218d0356e0b5765
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-03 18:40:54 +0300

    test4

--- lesson6/task4/index.js
+++ lesson6/task4/index.js
@@ -1,5 +1,5 @@
 const search = document.querySelector('.search__btn');
 const inputText = document.querySelector('.search__input');
 search.addEventListener('click', () => {
-  console.log(inputText.getAttribute('type'));
+  console.log(`${inputText.value}`);
 });

0ed50f7305f5eab9a188c0e5faed1c794ea9f9c7
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-03 18:30:40 +0300

    test3

--- lesson6/task3/index.js
+++ lesson6/task3/index.js
@@ -1,4 +1,8 @@
 const checkboxElem = document.querySelector('.task-status');
 checkboxElem.addEventListener('change', () => {
-  console.log(checkboxElem.value);
+  if (checkboxElem.checked) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
 });

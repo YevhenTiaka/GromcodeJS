2deb477284862056b0f7e2f367536edacc53bc2e
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-08 17:56:01 +0300

    test2

---
+++ lesson9/index.js
@@ -0,0 +1,24 @@
+const counterElem = document.querySelector('.counter');
+const counterValueElem = document.querySelector('.counter__value');
+const onCounterChange = (e) => {
+  const isButton = e.target.classList.contains('counter__button');
+  if (!isButton) {
+    return;
+  }
+  const { action } = e.target.dataset;
+  const oldValue = Number(counterValueElem.textContent);
+
+  const newValue = action === 'decrease' ? oldValue - 1 : oldValue + 1;
+  localStorage.setItem('counterValue', newValue);
+  counterValueElem.textContent = newValue;
+};
+counterElem.addEventListener('click', onCounterChange);
+const onStorageChange = (e) => {
+  counterValueElem.textContent = e.newValue;
+};
+window.addEventListener('storage', onStorageChange);
+
+const onDocumentLoaded = (e) => {
+  counterValueElem.textContent = -localStorage('counterValue') || 0;
+};
+document.addEventListener('DOMContentLoaded', onDocumentLoaded);

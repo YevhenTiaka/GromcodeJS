5c88fa309d0eef43f0b34bf9a756678c4ee35014
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-14 09:40:22 +0300

    test1

--- context/fetch response/index.js
+++ context/fetch response/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const userAvatarElem = document.querySelector('.user__avatar');
 const userNameElem = document.querySelector('.user__name');
 const userLocationElem = document.querySelector('.user__location');
@@ -5,24 +6,28 @@ const userLocationElem = document.querySelector('.user__location');
 const defaultAvatar = 'https://avatars3.githubusercontent.com/a';
 
 userAvatarElem.src = defaultAvatar;
+
+// 'https://api.github.com/users/USERNAME'
+// fetch and render userdata ...
+
 const fetchUserData = (userName) =>
   fetch(`https://api.github.com/users/${userName}`).then((response) =>
     response.json()
   );
 
 const renderUserData = (userData) => {
-  // eslint-disable-next-line camelcase
   const { avatar_url, name, location } = userData;
-  // eslint-disable-next-line camelcase
   userAvatarElem.src = avatar_url;
   userNameElem.textContent = name;
   userLocationElem.textContent = location ? `from ${location}` : '';
 };
-const showUserBtnElem = document.querySelector('.name-form__btn');
 
+const showUserBtnElem = document.querySelector('.name-form__btn');
 const userNameInputElem = document.querySelector('.name-form__input');
 
 const onSearchUser = () => {
   const userName = userNameInputElem.value;
   fetchUserData(userName).then((userData) => renderUserData(userData));
 };
+
+showUserBtnElem.addEventListener('click', onSearchUser);

dde692da5ae2971ddee0c53aed1f1cbe762678af
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-16 16:33:45 +0300

    test4

--- Lesson13/gateways.js
+++ Lesson13/gateways.js
@@ -1,17 +1,17 @@
 /* eslint-disable arrow-body-style */
 // eslint-disable-next-line arrow-body-style
-export const fetchUserData = (userName) => {
-  return fetch(`https://api.github.com/users/${userName}`).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Failed to load data');
-  });
+export const fetchUserData = async (userName) => {
+  const response = await fetch(`https://api.github.com/users/${userName}`);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error('Failed to load data');
 };
-export const fetchRepositories = (url) =>
-  fetch(url).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error('Failed to load data');
-  });
+
+export const fetchRepositories = async (url) => {
+  const response = await fetch(url);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Error('Failed to load data');
+};

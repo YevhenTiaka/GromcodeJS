d22facda2d6d192b4d68b65abcbb0504a8a82d37
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-20 19:27:59 +0300

    test1

---
+++ webpack/test1/src/profile/gateway.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-return-await */
+export default async function fetchUser(userId) {
+  const response = await fetch(`https://api.github.com/users/${userId}`);
+  if (!response.ok) {
+    throw new Error('Failed to get user data');
+  }
+  return await response.json();
+}

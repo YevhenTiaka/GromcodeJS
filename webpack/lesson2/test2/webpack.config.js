48fccb7382883362622266eb3eb9183293e6a630
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-21 15:15:36 +0300

    test2

---
+++ webpack/lesson2/test2/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.(jpg|png)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+};

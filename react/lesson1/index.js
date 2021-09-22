d098e5447e597fa3171a84e99ed095fa559a064c
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-22 13:32:54 +0300

    test1

---
+++ react/lesson1/index.js
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+// import ReactDOM from 'react-dom';
+
+const rootElement = document.querySelector('#root');
+
+const greetingElem = React.createElement(
+  'div',
+  { className: 'greeting' },
+  'Hello, React!'
+);
+ReactDOM.render(greetingElem, rootElement);

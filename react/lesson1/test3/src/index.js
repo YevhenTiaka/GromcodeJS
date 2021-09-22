17af03107f6582756a357769cef03ea6dce2b196
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-22 18:57:19 +0300

    test3

--- react/lesson1/test3/src/index.js
+++ react/lesson1/test3/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles.css';
 
 const rootElement = document.querySelector('#root');
+
 const element = (
   <div className='greeting'>
-    <div className='greeting__title'>Hello, world!</div>
-    <div className='greeting__text'>I'm learning React</div>
+    <h1 className='greeting__title'>Hello, world!</h1>
+    <p className='greeting__text'>I'm learning React</p>
   </div>
 );
-console.log(element);
-// eslint-disable-next-line no-undef
+
 ReactDOM.render(element, rootElement);

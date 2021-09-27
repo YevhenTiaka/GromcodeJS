ad484a8ca8197faf974e8212d4a31a5112de4b8a
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-27 12:12:57 +0300

    test3

--- react/lesson2/task1/src/index.js
+++ react/lesson2/task1/src/index.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM, { render } from 'react-dom';
 
 import './styles.css';
 
 const rootElement = document.querySelector('#root');
-const seconds = new Date().getSeconds();
+const renderSeconds = (time) => {
+  const seconds = new Date(time).getSeconds();
 
-const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
 
-const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
-const styles = {
-  color: textColor,
-  backgroundColor,
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+  const styles = {
+    color: textColor,
+    backgroundColor,
+  };
+
+  const element = (
+    <div className='seconds' style={styles}>
+      Now is {seconds}
+    </div>
+  );
+  ReactDOM.render(element, rootElement);
 };
-
-const element = (
-  <div className='seconds' style={styles}>
-    {seconds}
-  </div>
-);
-ReactDOM.render(element, rootElement);
+renderSeconds(new Date());

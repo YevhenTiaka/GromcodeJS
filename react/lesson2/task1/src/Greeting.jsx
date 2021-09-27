a910750aa0319133c108ebcc742a9789f762d2a8
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-27 17:37:29 +0300

    test1

--- react/lesson2/task1/src/Greeting.jsx
+++ react/lesson2/task1/src/Greeting.jsx
@@ -1,11 +1,12 @@
-import moment from 'moment';
 import React from 'react';
+import moment from 'moment';
 
-const rootElement = document.querySelector('#root');
 const Greeting = (props) => {
   const age = moment().diff(moment(props.birthDate), 'Years');
+
   return (
-    <div className='greeting'>{`My name is ${props.firstName}  ${props.lastName} . I'm ${age} years old`}</div>
+    <div className='greeting'>{`My name is ${props.firstName} ${props.lastName}. I'm ${age} years old`}</div>
   );
 };
+
 export default Greeting;
--- react/lesson2/task1/src/index.jsx
+++ react/lesson2/task1/src/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Greeting from './Greeting';
-import './styles.scss';
+import './index.scss';
 
 const rootElement = document.querySelector('#root');
 
 ReactDOM.render(
   <Greeting
-    firtsName='John'
+    firstName='John'
     lastName='Doe'
     birthDate={new Date('2001-01-01T11:11:11.819Z')}
   />,

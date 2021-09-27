b3ce18f540b45112eba61a34efac21a9301c907b
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-27 16:51:44 +0300

    test1

--- react/lesson2/task1/src/Comment.jsx
+++ react/lesson2/task1/src/Comment.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import './comment.scss';
-import moment from 'moment';
+import './comments.scss';
 import UserInfo from './UserInfo';
+import moment from 'moment';
+
 const formatDate = (date) => moment(date).format('DD MMM YYYY');
 
 function Comment(props) {
   return (
     <div className='comment'>
-      <UserInfo user={props.user} />
+      <UserInfo user={props.author} />
       <div className='comment__text'>{props.text}</div>
       <div className='comment__date'>{formatDate(props.date)}</div>
     </div>
--- react/lesson2/task1/src/index.jsx
+++ react/lesson2/task1/src/index.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react';
-import './styles.scss';
-import Comment from './Comment';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
 
 const rootElement = document.querySelector('#root');
-const userInfo = {
-  name: 'Tom',
-  avatarUrl: '',
-};
-ReactDOM.render(
-  <Comment
-    user={userInfo}
-    text='Good job!'
-    date={new Date('2019-01-01T11:32:19.566Z')}
-  />,
-  rootElement
-);
+
+ReactDOM.render(<App />, rootElement);

59538e7e0868c69d88c61c6d16b11cb85ac45995
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-27 12:09:46 +0300

    test3

--- react/lesson2/task1/src/index.js
+++ react/lesson2/task1/src/index.js
@@ -1,37 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './styles.css';
+
 const rootElement = document.querySelector('#root');
-const style = { color: 'red', fontWeight: '700' };
+const seconds = new Date().getSeconds();
+
+const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+
+const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+const styles = {
+  color: textColor,
+  backgroundColor,
+};
+
 const element = (
-  <>
-    <main className='page'>
-      <form className='login-form'>
-        <h1 className='form-title'>Profile</h1>
-        <div className='form-control'>
-          <label className='form-label' htmlFor='name'>
-            Name
-          </label>
-          <input
-            className='form-input'
-            type='text'
-            id='name'
-            name='name'
-            value='anonymous'
-          />
-        </div>
-        <div className='form-control'>
-          <label className='form-label' id='age' htmlFor='age'>
-            Age
-          </label>
-          <input className='form-input' type='number' value='17' name='age' />
-          <span style={style}>To young</span>
-        </div>
-        <button className='submit-button' type='submit'>
-          Submit
-        </button>
-      </form>
-    </main>
-  </>
+  <div className='seconds' style={styles}>
+    {seconds}
+  </div>
 );
 ReactDOM.render(element, rootElement);

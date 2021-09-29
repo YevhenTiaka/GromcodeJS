aa4f296cc6d6d3058f254112067bccf0d789ede2
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-29 15:00:19 +0300

    test5

---
+++ react/Events/task5/src/Toggler.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class Toggler extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      buttonValue: 'Off',
+    };
+  }
+
+  handleClick = () => {
+    this.state.buttonValue === 'Off'
+      ? this.setState({ buttonValue: 'On' })
+      : this.setState({ buttonValue: 'Off' });
+  };
+
+  render() {
+    return (
+      <button className='button' onClick={this.handleClick}>
+        {this.state.buttonValue}
+      </button>
+    );
+  }
+}
+
+export default Toggler;

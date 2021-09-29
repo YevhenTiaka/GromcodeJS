488cd186ab9e8654b2acfe5fab8e0a0c25d2b318
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-29 11:54:08 +0300

    test2

--- react/Events/src/GoodButton.jsx
+++ react/Events/src/GoodButton.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-const GoodButton = () => {
-  return (
-    <button
-      className='fancy-button'
-      onClick={() => {
-        alert('Good job!');
-      }}
-    >
-      Click me!
-    </button>
-  );
-};
+class GoodButton extends React.Component {
+  handleClick() {
+    alert('Good job!');
+  }
+  render() {
+    return (
+      <button className='fancy-button' onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
 export default GoodButton;

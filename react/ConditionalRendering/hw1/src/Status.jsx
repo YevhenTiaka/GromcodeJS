bf2327dcbed7f37f4c468fc8a02720440869b3e4
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-09-30 16:23:15 +0300

    hw1

---
+++ react/ConditionalRendering/hw1/src/Status.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Online from './Online';
+import Offline from './Offline';
+
+class Status extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='status'>
+        {this.props.isOnline ? <Online /> : <Offline />}
+      </div>
+    );
+  }
+}
+
+export default Status;

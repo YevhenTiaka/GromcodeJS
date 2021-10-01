38d807d4488bb7abfce6dd0437e6d0340ada881b
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-01 15:31:12 +0300

    test1

---
+++ react/List/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import NumbersList from './NumbersList';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<NumbersList />, rootElement);

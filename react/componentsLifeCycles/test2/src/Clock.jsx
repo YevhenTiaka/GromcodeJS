9f4c1db6919fe1859ed66306b7f5f4e50d8d3775
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-04 18:40:18 +0300

    test2

--- react/componentsLifeCycles/test2/src/Clock.jsx
+++ react/componentsLifeCycles/test2/src/Clock.jsx
@@ -4,23 +4,23 @@ import moment from 'moment';
 const getTimeWithOffset = (offset) => {
   const currentTime = new Date();
   const utcOffset = currentTime.getTimezoneOffset() / 60;
-  return moment(
-    new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset))
-  ).format('h:mm:ss: A');
+  return new Date(
+    currentTime.setHours(currentTime.getHours() + offset + utcOffset)
+  );
 };
 
 class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      date: getTimeWithOffset(this.props.offset),
+      date: moment(getTimeWithOffset(this.props.offset)).format('LTS'),
     };
   }
 
   componentDidMount() {
     this.interval = setInterval(() => {
       this.setState({
-        date: getTimeWithOffset(this.props.offset),
+        date: moment(getTimeWithOffset(this.props.offset)).format('LTS'),
       });
     }, 1000);
   }

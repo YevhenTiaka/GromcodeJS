cb8fc094591423f579315df33e92aba53190393e
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-05 11:09:52 +0300

    hw1

--- react/componentsLifeCycles/test4/src/ConnectionStatus.jsx
+++ react/componentsLifeCycles/test4/src/ConnectionStatus.jsx
@@ -3,25 +3,21 @@ import React from 'react';
 class ConnectionStatus extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      online: 'true',
-    };
+    this.state = { online: window.navigator.onLine };
   }
-  toggle = () => {
-    const { online } = event.target.navigator;
-    this.setState({
-      online: online,
-    });
-  };
   componentDidMount() {
-    window.addEventListener('online', this.toggle);
-    window.addEventListener('offline', this.toggle);
-  }
-  componentWillUnmount() {
-    window.removeEventListener('online', this.toggle);
-    window.removeEventListener('offline', this.toggle);
+    window.addEventListener('offline', this.handleNetworkChange);
+    window.addEventListener('online', this.handleNetworkChange);
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('offline', this.handleNetworkChange);
+    window.removeEventListener('online', this.handleNetworkChange);
+  }
+
+  handleNetworkChange = () => {
+    this.setState({ online: window.navigator.onLine });
+  };
   render() {
     return this.state.online ? (
       <div className='status '>online</div>

f3127a73069887ef61c3acb35218f616bcadb29a
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-15 10:43:34 +0300

    test2

--- react/ReactRoute/task2/src/App.jsx
+++ react/ReactRoute/task2/src/App.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
-import Products from './Products';
-import Home from './Home';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import Home from './Home.jsx';
+import Products from './Products.jsx';
 
-const App = () => {
-  return (
-    <div className="page">
-      <BrowserRouter>
-        <ul className="navigation">
-          <li className="navigation__item">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="navigation__item">
-            <Link to="/products">Products</Link>
-          </li>
-        </ul>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/products" component={Products} />
-      </BrowserRouter>
-    </div>
-  );
-};
+const App = () => (
+  <div className="page">
+    <BrowserRouter>
+      <ul className="navigation">
+        <li className="navigation__item">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="navigation__item">
+          <Link to="/products">Products</Link>
+        </li>
+      </ul>
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route path="/products" component={Products} />
+    </BrowserRouter>
+  </div>
+);
 
 export default App;

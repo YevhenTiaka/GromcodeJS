40b43e4d264055a858da3966f7e0c777d1ba79ed
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-15 12:35:14 +0300

    test1

---
+++ react/ReactHooks/task1/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Counter from './Counter';
+ReactDOM.render(<Counter />, document.querySelector('#root'));

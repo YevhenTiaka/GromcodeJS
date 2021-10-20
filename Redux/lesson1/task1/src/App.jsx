8934b9387a4e35c00301f2c6b651bc4710e3662f
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-20 23:26:14 +0300

    test1

---
+++ Redux/lesson1/task1/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import ThemedButton from './ThemedButton.jsx';
+import { themes, ThemeContext } from './themes-context.js';
+
+class App extends React.Component {
+  state = {
+    theme: themes.dark,
+  };
+
+  // eslint-disable-next-line class-methods-use-this
+  toggleTheme = () => {
+    const newTheme = this.state.theme === themes.dark ? themes.light : themes.dark;
+    this.setState({
+      theme: newTheme,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
+
+          <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+        </ThemeContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ Redux/lesson1/task1/src/ThemedButton.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ThemeContext } from './themes-context';
+
+class ThemedButton extends React.Component {
+  render() {
+    return (
+      <button
+        {...this.props}
+        style={{
+          backgroundColor: this.context.backgroundColor,
+          color: this.context.fontColor,
+        }}
+        className="btn"
+      ></button>
+    );
+  }
+}
+
+ThemedButton.contextType = ThemeContext;
+export default ThemedButton;

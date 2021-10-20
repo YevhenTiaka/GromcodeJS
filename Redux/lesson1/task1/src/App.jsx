ba262b51f873e97a5e6dc3273ae66e834db4ad97
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-20 23:31:39 +0300

    test1

--- Redux/lesson1/task1/src/App.jsx
+++ Redux/lesson1/task1/src/App.jsx
@@ -21,9 +21,8 @@ class App extends React.Component {
       <div>
         <ThemeContext.Provider value={this.state.theme}>
           <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
-
-          <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
         </ThemeContext.Provider>
+        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
       </div>
     );
   }

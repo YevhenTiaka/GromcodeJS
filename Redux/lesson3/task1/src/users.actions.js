ab4b236c0967ca79f9f379e0237c8994023146b7
YevhenTiaka <87383980+YevhenTiaka@users.noreply.github.com>
2021-10-25 14:18:53 +0300

    test1

--- Redux/lesson3/task1/src/users.actions.js
+++ Redux/lesson3/task1/src/users.actions.js
@@ -1,6 +1,6 @@
-export const ADD_USER = 'USER/ADD_USER';
-export const DELETE_USER = 'USER/DELETE_USER';
-export const UPDATE_USER = 'USER/UPDATE_USER';
+export const ADD_USER = 'USERS/ADD_USER';
+export const DELETE_USER = 'USERS/DELETE_USER';
+export const UPDATE_USER = 'USERS/UPDATE_USER';
 
 export const addUser = userData => ({
   type: ADD_USER,
@@ -8,14 +8,12 @@ export const addUser = userData => ({
     userData,
   },
 });
-
 export const deleteUser = userId => ({
   type: DELETE_USER,
   payload: {
     userId,
   },
 });
-
 export const updateUser = (userId, userData) => ({
   type: UPDATE_USER,
   payload: {
